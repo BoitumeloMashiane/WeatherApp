84adb15f71465d441ff0ba105780ae6b4abe1596
BoitumeloMashiane <boitumelo.bibo@gmail.com>
2023-10-16 11:32:26 +0200

    Code clean up and finishing up responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 import WeeklyWeather from './weeklyweather';
-import { DAYS,MONTHS } from "./constants";
 import TodayWeather from './todayweather';
 
 const API_URL = 'http://api.openweathermap.org/data/2.5/forecast?=&appid=81c6adb9901e737585d9280aace6a2aa&units=metric';
@@ -13,7 +12,7 @@ const App = () =>{
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(()=>{
-    searchCity('Siyabuswa');
+    searchCity('Centurion');
   },[]);
 
   const searchCity = async (cityname) =>{
@@ -22,33 +21,37 @@ const App = () =>{
     setCity(data);
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchCity(searchTerm);
+  };
+
     return (
-      <>
-      <div className='container mx-auto relative'>
-        <span className=" absolute w-6 bottom-2 left-2">
-          <img src="img/icons8-search.svg" alt="" />
-        </span>
-        <input className="placeholder:italic mt-10 placeholder:text-slate-400 bg-white w-full border-b-2 border-slate-300 py-2 pl-11 pr-3 shadow-sm focus:outline-none focus:border-sky-500 sm:text-sm" placeholder="Search for a city..." type="text" name="search" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} />
-        <button className='absolute bg-yellow-600 bottom-0 right-0 rounded-sm py-2 px-4 text-white font-bold hover:bg-yellow-500' onClick={()=>searchCity(searchTerm)}>Search</button>
+      <div className='h-screen'>
+        <form onSubmit={handleSubmit} className='container mx-auto relative'>
+          <span className=" absolute w-6 bottom-2 left-2">
+            <img src="icons8-search.svg" alt="" />
+          </span>
+          <input className="placeholder:italic mt-10 placeholder:text-slate-400 bg-white w-full border-b-2 border-slate-300 py-2 pl-11 pr-3 shadow-sm focus:outline-none focus:border-red-500 focus:bg-white sm:text-sm" placeholder="Search for a city..." type="text" name="search" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} />
+        </form>
+        {city!=null && city.cod == 200
+          ?(
+            <>
+              <TodayWeather city={city}/>
+              <section className='container mx-auto flex flex-row justify-between h-1/3 mt-2 md:mt-24 border-solid border-y-2 border-slate-100'>
+                <WeeklyWeather cityname={city.list[0]}/>
+                <WeeklyWeather cityname={city.list[8]}/>
+                <WeeklyWeather cityname={city.list[16]}/>
+                <WeeklyWeather cityname={city.list[24]}/>
+                <WeeklyWeather cityname={city.list[32]}/>
+              </section>
+            </>
+          ):(
+            <>
+              <h1 className='text-center italic text-[3.5rem] text-amber-700 pt-24'>City Not Found!</h1>
+            </>
+          )}
       </div>
-      {city!=null && city.cod == 200
-        ?(
-          <>
-            <TodayWeather city={city}/>
-            <section className='container mx-auto flex flex-row justify-between'>
-              <WeeklyWeather cityname={city.list[0]}/>
-              <WeeklyWeather cityname={city.list[8]}/>
-              <WeeklyWeather cityname={city.list[16]}/>
-              <WeeklyWeather cityname={city.list[24]}/>
-              <WeeklyWeather cityname={city.list[32]}/>
-            </section>
-          </>
-        ):(
-          <>
-            <h1>City Not Found</h1>
-          </>
-        )}
-      </>
     )
 
 }
--- src/weeklyweather.jsx
+++ src/weeklyweather.jsx
@@ -4,14 +4,13 @@ import { DAYS,MONTHS } from "./constants";
 
 const WeeklyWeather = ({cityname}) =>{
     return (
-        <div className='flex flex-col items-center'>
-        <h1>{DAYS[new Date(cityname.dt_txt).getDay()]}</h1>
-        <p>{new Date(cityname.dt_txt).getDate()} {MONTHS[new Date(cityname.dt_txt).getMonth() -1]}</p>
-        <p>{cityname.main.temp}&deg;C</p>
+        <div className='flex flex-col items-center justify-center'>
+        <h1 className="text-red-500 font-semibold text-lg">{DAYS[new Date(cityname.dt_txt).getDay()]}</h1>
+        <p className="font-semibold italic">{new Date(cityname.dt_txt).getDate()} {MONTHS[new Date(cityname.dt_txt).getMonth() -1]}</p>
+        <p className="leading-8 text-slate-500">{cityname.main.temp}&deg;C</p>
         <img src={`https://openweathermap.org/img/wn/${cityname.weather[0].icon}@2x.png`} alt="" />
         </div>
     );
 }
-
 
 export default WeeklyWeather;
